ea559286c52fc47d22f3b280e57f093870124fe2
timf34 <farrelti@tcd.ie>
2024-04-08 15:29:18 +0100

    refactored to improve image saving

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
+import datetime
+import requests
+import os
+
+from io import BytesIO
 from openai import OpenAI
 from openai import RateLimitError
-import datetime
 from PIL import Image
-import requests
-from io import BytesIO
+from typing import Optional
 
 from keys import OPENAI_API_KEY
 
+
 client = OpenAI(api_key=OPENAI_API_KEY)
+
+IMAGE_PATH: str = "dalle_generated_images"
+
+def save_image(url, file_path):
+    response = requests.get(url)
+    image = Image.open(BytesIO(response.content))
+    image.save(file_path)
+
+
+def save_image(url, directory="dalle_generated_images") -> Optional[str]:
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    file_name = f"room_image_{timestamp}.png"
+    file_path = os.path.join(directory, file_name)
+    try:
+        response = requests.get(url)
+        image = Image.open(BytesIO(response.content))
+        image.save(file_path)
+        print(f"Image saved as {file_path}")
+        return file_path
+    except Exception as e:
+        print(f"An error occurred while saving the image: {e}")
+        return None
+
 
 
 def is_room_description(description):
@@ -42,14 +72,10 @@ def generate_room_image(prompt):
     image_data = response.data[0].url
     return image_data
 
-
-def save_image(url, file_path):
-    response = requests.get(url)
-    image = Image.open(BytesIO(response.content))
-    image.save(file_path)
-
-
-def main():
+def generate_image_path() -> str:
+    """
+    Asks the user to describe a room, then returns the image path to resulted generated room image
+    """
     while True:
         try:
             room_description = input("Please describe a simple room: ")
@@ -63,11 +89,10 @@ def main():
             break  # Exit the loop and the program since you can't proceed without API access
 
     image_url = generate_room_image(room_description)
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    file_path = f"room_image_{timestamp}.png"
-    save_image(image_url, file_path)
-    print(f"Image saved as {file_path}")
+    file_path = save_image(image_url, IMAGE_PATH)
+    return file_path
 
 
 if __name__ == "__main__":
-    main()
+    image_path = generate_image_path()
+    print(image_path)
