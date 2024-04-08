4f77629c7ae38a7d6e7b4f703eb7f547cd83690e
timf34 <farrelti@tcd.ie>
2024-04-08 15:29:08 +0100

    removed unnecessary parts of pipeline - should really speed up image generation

--- generate_scene.py
+++ generate_scene.py
@@ -45,21 +45,7 @@ def main(args):
     # save outputs before completion
     pipeline.clean_mesh()
     intermediate_mesh_path = pipeline.save_mesh("after_generation.ply")
-    save_poisson_mesh(intermediate_mesh_path, depth=args.poisson_depth, max_faces=args.max_faces_for_poisson)
-
-    # run completion
-    pipeline.args.update_mask_after_improvement = True
-    pipeline.complete_mesh(offset=offset)
-    pipeline.clean_mesh()
-
-    # Now no longer need the models
-    pipeline.remove_models()
-
-    # save outputs after completion
-    final_mesh_path = pipeline.save_mesh()
-
-    # run poisson mesh reconstruction
-    mesh_poisson_path = save_poisson_mesh(final_mesh_path, depth=args.poisson_depth, max_faces=args.max_faces_for_poisson)
+    mesh_poisson_path = save_poisson_mesh(intermediate_mesh_path, depth=args.poisson_depth, max_faces=args.max_faces_for_poisson)
 
     # save additional output
     pipeline.save_animations()
